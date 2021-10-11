9dca8246d2230438eb68a66b7720f3c78227ebaf
Ryan Blyth <ryan.blyth@hotmail.com>
2021-10-11 18:10:17 +0000

    Add employee object and get_info function

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class Employee:
+    def __init__(self, number, name, age, email):
+        self.number = number
+        self.name = name
+        self.age = age
+        self.email = email
+
+    def get_employee_info(self):
+        print(
+            f"""
+            Employee Number: {self.number}
+            Name: {self.name}
+            Age: {self.age}
+            Email: {self.email}
+            """
+        )
+
+
+ryan = Employee(1, "Ryan", 22, "ryan@company.com")
+
+ryan.get_employee_info()
