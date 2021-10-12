b8d405caa22f245981db0a25dee62bc6b08a9fda
Ryan Blyth <ryan.blyth@hotmail.com>
2021-10-12 16:52:52 +0000

    Add user input loop for adding employee

--- run.py
+++ run.py
@@ -5,12 +5,18 @@ employees = []
 
 
 class Employee:
+    """
+        Creates an instance of an Employee.
+    """
     def __init__(self, number, name, age, email):
         self.number = number
         self.name = name
         self.age = age
         self.email = email
 
+    """
+        Prints employee info in terminal.
+    """
     def get_employee_info(self):
         print(
             f"""
@@ -25,6 +31,12 @@ class Employee:
 ryan = Employee(1, "Ryan", 22, "ryan@company.com")
 
 
+def user_action():
+    print("Add new employee or check employee details?")
+    action = input("\n")
+    return action
+    
+
 def add_new_employee():
     number = random.randint(0, 10000)
     name = input("Employee name: \n")
@@ -32,11 +44,20 @@ def add_new_employee():
     email = input("Employee email: \n")
 
     new_employee = Employee(number, name, age, email)
-    return new_employee
+    employees.append(new_employee)
 
 
-new_employee_ = add_new_employee()
-employees.append(new_employee_)
+while True:
+    print("Add new employee (A) or check employee details (C)?")
+    answer = input("\n").lower()
+    if answer == "a":
+        add_new_employee()
+        break
+    elif answer == "c":
+        pass
+    else:
+        print("Invalid input, please try again. Input A/C only.")
+        continue
+
 print(employees)
-
-employees[0].get_employee_info()
+print(employees[0].get_employee_info())
